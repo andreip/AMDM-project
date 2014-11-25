6db34c085cb695446d90722e87759d08664c94e2
Andrei Petre <p31andrei@gmail.com>
2014-11-25 16:39:01 +0200

    Add python code.

---
+++ main.py
@@ -0,0 +1,22 @@
+filename = 'tweets_15m.txt'
+tweets_max = 1000
+read_chunk = 100
+
+tweet_terms = {}
+with open(filename) as f:
+    read_tweets = 0
+    while read_tweets < tweets_max:
+        tweets = f.readlines(read_chunk)
+        # Tweets are finished.
+        if not tweets:
+            break
+        read_tweets += read_chunk
+        for raw_tweet in tweets:
+            tweet = raw_tweet.strip('\n').split('\t')
+            for word in set(tweet):
+                tweet_terms[word] = tweet_terms.get(word, 0) + 1
+
+terms_in_tweets_count = {}
+for (word,n) in tweet_terms.iteritems():
+    terms_in_tweets_count[n] = terms_in_tweets_count.get(n, 0) + 1
+
