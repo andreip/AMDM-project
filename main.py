7e4b92713cacb2f7752f6e913f17a3b6a0dbf4b2
Andrei Petre <p31andrei@gmail.com>
2014-11-25 19:09:00 +0200

    Plot all 3 versions.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import pylab as pl
 
 filename = 'tweets_15m.txt'
-tweets_max = 10000
+tweets_max = 1000
 read_chunk = 100
 
 tweet_terms = {}
@@ -20,14 +21,52 @@ with open(filename) as f:
             for word in set(tweet):
                 tweet_terms[word] = tweet_terms.get(word, 0) + 1
 
+max_index = 0
 terms_in_tweets_count = {}
 for (word,n) in tweet_terms.iteritems():
     terms_in_tweets_count[n] = terms_in_tweets_count.get(n, 0) + 1
+    max_index = max(max_index, n)
 
 """omas"""
 
-pl.plot(terms_in_tweets_count.keys(), terms_in_tweets_count.values(), 'rx')
-pl.yscale('log')
-pl.xscale('log')
-pl.xlabel('appears in k tweets')
-pl.show()
+# First plot with number of terms that appear in k tweets.
+def plot1():
+    pl.figure(1)
+    pl.plot(terms_in_tweets_count.keys(), terms_in_tweets_count.values(), 'rx')
+    pl.yscale('log')
+    pl.xscale('log')
+    pl.xlabel('appears in k tweets')
+    pl.show()
+
+# Second plot with cummulative distribution, so number of tweets that appear
+# in k or less tweets.
+def plot2():
+    pl.figure(2)
+    pl.clf()
+    accumulator = 0
+    for (n, appearances) in iter(sorted(terms_in_tweets_count.iteritems())):
+        accumulator += appearances
+        pl.plot(n, accumulator, 'rx')
+    pl.yscale('log')
+    pl.xscale('log')
+    pl.xlabel('appears in k or less tweets')
+    pl.show()
+
+# Third plot which plots number of times a word appears in tweets but in
+# descending order by appearances.
+def plot3():
+    pl.figure(3)
+    pl.clf()
+    iter_sorted = iter(sorted(tweet_terms.iteritems(), key=lambda x: x[1],
+    reverse=True))
+    for i, (word, appearances) in enumerate(iter_sorted):
+        pl.plot(i+1, appearances, 'rx')
+    pl.yscale('log')
+    pl.xscale('log')
+    pl.ylabel('Appearances of word j in tweets')
+    pl.xlabel('j')
+    pl.show()
+
+plot1()
+plot2()
+plot3()
